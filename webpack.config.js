33b45fecfe9decf68805f7ac1f46590f6d7d70e5
houzehang <houzehang@youshi-inc.com>
2018-11-05 01:51:14 +0800

    使用模块热替换 HMR 来处理 CSS

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ let ExtractTextPlugin = require('extract-text-webpack-plugin');
 ////======== 清除旧的编译生成文件 ========
 let CleanWebpackPlugin = require('clean-webpack-plugin');
 
+let webpack = require('webpack');
 module.exports = {
 	entry: {
 		'app.bundle': './src/app.js',
@@ -13,17 +14,21 @@ module.exports = {
 	},
 	output: {
 		path: __dirname + '/dist',
-		filename: '[name].[chunkhash].js'
+		// filename: '[name].[chunkhash].js'
+		////======== 开启模块热替换时需要使用hash(每个文件的hash码统一) ========
+		filename: '[name].[hash].js'
 	},
 	module: {
 		rules: [{
 			test: /\.css$/,
 			////======== 使用ExtractTextPlugin前 ======== 
-			// use: ['style-loader', 'css-loader']
-			use: ExtractTextPlugin.extract({
-				fallback: 'style-loader',
-				use: ['css-loader'],
-			})
+			use: ['style-loader', 'css-loader']
+
+			////======== 使用ExtractTextPlugin前(如果开启了模块热替换，不可以使用如下的代码) ======== 
+			// use: ExtractTextPlugin.extract({
+			// 	fallback: 'style-loader',
+			// 	use: ['css-loader'],
+			// })
 		}, {
 			test: /\.js$/,
 			loader: 'babel-loader',
@@ -67,12 +72,24 @@ module.exports = {
 			// 排除某些依赖
 			// excludeChunks:['app.bundle']
 		}),
-		new ExtractTextPlugin('styles.css'),
-		new CleanWebpackPlugin(['dist'])
+		////======== 开启模块热替换时需要关闭掉 ========
+		// new ExtractTextPlugin('styles.css'),
+		new ExtractTextPlugin({
+			filename: 'styles.css',
+			disable: true
+		}),
+
+		new CleanWebpackPlugin(['dist']),
+
+		////======== 模块热替换 ========
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
 	],
 	devServer: {
 		port: 9000,
 		////======== 自动打开 ========
-		open: true
+		open: true,
+		////======== 开启模块热替换（内容修改时不会重新加载界面） ========
+		hot: true
 	}
 };
